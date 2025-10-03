205335d18911da0831d67f4edb4248722e9bedcf
rsn007 <rsn007@gmail.com>
2025-10-03 20:20:05 +0000

    Added room creating process

--- app.js
+++ app.js
@@ -7,8 +7,18 @@ const createBtn = document.getElementById('createRoomBtn');
 const joinBtn = document.getElementById('joinRoomBtn');
 
 createBtn.addEventListener('click', () => {
-  statusBar.textContent = 'Створення кімнати... (логіка Firebase буде тут)';
-  // TODO: логіка створення кімнати у базі
+  statusBar.textContent = 'Створення кімнати...';
+  const newRoomRef = db.ref('rooms').push();
+  newRoomRef.set({
+    createdAt: firebase.database.ServerValue.TIMESTAMP,
+    players: 1,
+    // Add other initial room data here
+  }).then(() => {
+    statusBar.textContent = `Кімнату #${newRoomRef.key} створено!`;
+    // TODO: Add logic to join the room
+  }).catch(error => {
+    statusBar.textContent = `Помилка створення кімнати: ${error.message}`;
+  });
 });
 
 joinBtn.addEventListener('click', () => {
@@ -18,26 +28,26 @@ joinBtn.addEventListener('click', () => {
 
 function updateRoomList(rooms) {
   roomList.innerHTML = '';
-  if (!rooms || rooms.length === 0) {
+  if (!rooms || Object.keys(rooms).length === 0) {
     roomList.textContent = 'Активних кімнат поки немає';
     return;
   }
-  rooms.forEach(room => {
+  Object.keys(rooms).forEach(roomId => {
+    const room = rooms[roomId];
     const div = document.createElement('div');
     div.className = 'room-item';
-    div.textContent = `Кімната #${room.id} - ${room.players}/2 гравців`;
+    div.textContent = `Кімната #${roomId} - ${room.players}/2 гравців`;
     div.tabIndex = 0;
     div.addEventListener('click', () => {
-      statusBar.textContent = `Приєднуємось до кімнати #${room.id}...`;
+      statusBar.textContent = `Приєднуємось до кімнати #${roomId}...`;
       // TODO: додати логіку підключення до кімнати
     });
     roomList.appendChild(div);
   });
 }
 
-// Демонстрація тестових кімнат
-updateRoomList([
-  {id: 101, players: 1},
-  {id: 102, players: 0},
-  {id: 103, players: 2}
-]);
+const roomsRef = db.ref('rooms');
+roomsRef.on('value', (snapshot) => {
+    const rooms = snapshot.val();
+    updateRoomList(rooms);
+});
